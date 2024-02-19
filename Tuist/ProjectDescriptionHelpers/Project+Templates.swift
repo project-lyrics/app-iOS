f0b14af14671aa35f53906fdf3e2bdd0a7b24411
derrickkim0109 <derrickkim0109@naver.com>
2024-02-19 16:18:47 +0900

    feat: Project 객체 init 메서드 추가

---
+++ Tuist/ProjectDescriptionHelpers/Project+Templates.swift
@@ -0,0 +1,29 @@
+import ProjectDescription
+
+public extension Project {
+    static func makeModule(
+        name: String,
+        organizationName: String? = nil,
+        options: Project.Options = .options(),
+        packages: [Package] = [],
+        settings: Settings? = nil,
+        targets: [Target],
+        schemes: [Scheme] = [],
+        fileHeaderTemplate: FileHeaderTemplate? = nil,
+        additionalFiles: [FileElement] = [],
+        resourceSynthesizers: [ResourceSynthesizer] = []
+    ) -> Self {
+        return .init(
+            name: name,
+            organizationName: organizationName,
+            options: options,
+            packages: packages,
+            settings: settings,
+            targets: targets,
+            schemes: schemes,
+            fileHeaderTemplate: fileHeaderTemplate,
+            additionalFiles: additionalFiles,
+            resourceSynthesizers: resourceSynthesizers
+        )
+    }
+}
