7241cecaa2a01bb9bc91c306a773b8c08e63ff28
derrickkim0109 <derrickkim0109@naver.com>
2024-02-19 16:38:08 +0900

    add: Shared Sample 추가
    - Font Pretendard 추가
    - Colors, Font, Images객체 추가
    - Utils Sample 객체 추가

---
+++ Projects/Shared/DesignSystem/Sources/Images.swift
@@ -0,0 +1,8 @@
+//
+//  Images.swift
+//  SharedDesignSystem
+//
+//  Created by Derrickkim on 2024/02/18.
+//
+
+import Foundation
---
+++ Projects/Shared/Sources/SharedExport.swift
@@ -0,0 +1,6 @@
+//
+//  SharedRoot.swift
+//  Shared
+//
+//  Created by Derrick kim on 2/19/24.
+//
