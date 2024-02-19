5feb70e336a760b59479e5ad09eeb97d771bebb1
derrickkim0109 <derrickkim0109@naver.com>
2024-02-19 16:18:02 +0900

    add: SourceFileList Path 프로퍼티 추가

---
+++ Tuist/ProjectDescriptionHelpers/SourceFilesList+Template.swift
@@ -0,0 +1,16 @@
+//
+//  SourceFilesList+Template.swift
+//  ProjectDescriptionHelpers
+//
+//  Created by Derrick kim on 2/16/24.
+//
+
+import ProjectDescription
+
+public extension SourceFilesList {
+    static let exampleSources: SourceFilesList = "Example/Sources/**"
+    static let interface: SourceFilesList = "Interface/Sources/**"
+    static let sources: SourceFilesList = "Sources/**"
+    static let testing: SourceFilesList = "Testing/Sources/**"
+    static let tests: SourceFilesList = "Tests/Sources/**"
+}
