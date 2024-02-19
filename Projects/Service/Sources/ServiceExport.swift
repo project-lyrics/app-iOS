b3ed562936f51bfae2fa7da3fafc6ffb991f730b
derrickkim0109 <derrickkim0109@naver.com>
2024-02-19 16:35:19 +0900

    add: Service User Sample 객체 추가

---
+++ Projects/Service/Sources/ServiceExport.swift
@@ -0,0 +1,8 @@
+//
+//  ServiceExport.swift
+//  Service
+//
+//  Created by Derrick kim on 2/19/24.
+//
+
+@_exported import ServiceUserInterface
---
+++ Projects/Service/Sources/ServiceRoot.swift
@@ -0,0 +1,8 @@
+//
+//  ServiceRoot.swift
+//  Service
+//
+//  Created by Derrick kim on 2/19/24.
+//
+
+// ServiceRoot
