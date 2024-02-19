ef6347e6681df21418b02964d5fe141633ef1fbb
derrickkim0109 <derrickkim0109@naver.com>
2024-02-19 16:17:25 +0900

    add: SPM 관리를 위한 객체 추가

---
+++ Tuist/ProjectDescriptionHelpers/SPM.swift
@@ -0,0 +1,26 @@
+//
+//  SPM.swift
+//  ProjectDescriptionHelpers
+//
+//  Created by Derrick kim on 2/17/24.
+//
+
+import ProjectDescription
+
+extension TargetDependency {
+    public struct SPM {}
+}
+
+public extension TargetDependency.SPM {
+    static let Kingfisher = Self.package(product: "Kingfisher")
+    static let FlexLayout = Self.package(product: "FlexLayout")
+    static let PinLayout = Self.package(product: "PinLayout")
+
+    private static func external(_ name: String) -> TargetDependency {
+        return TargetDependency.external(name: name)
+    }
+
+    private static func package(product: String) -> TargetDependency {
+        return TargetDependency.package(product: product)
+    }
+}
