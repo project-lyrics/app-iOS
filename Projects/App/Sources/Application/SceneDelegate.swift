5bb3851590801824e2f651c421a223d92500a88b
derrickkim0109 <derrickkim0109@naver.com>
2024-02-19 16:32:31 +0900

    feat: App 모듈 초기 설정
    - Info.plist 설정
    - Package 추가
    - AppDelegate, SceneDelegate 추가
    - entitlements 파일 추가
    - LaunchScreen 추가
    - Workspace 파일 추가

---
+++ Projects/App/Sources/Application/SceneDelegate.swift
@@ -0,0 +1,38 @@
+//
+//  SceneDelegate.swift
+//  Lyrics
+//
+//  Created by Derrick kim on 2/19/24.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    var window: UIWindow?
+
+    func scene(
+        _ scene: UIScene,
+        willConnectTo session: UISceneSession,
+        options connectionOptions: UIScene.ConnectionOptions
+    ) {
+        guard let windowScene = (scene as? UIWindowScene) else {
+            return
+        }
+
+        window = UIWindow(windowScene: windowScene)
+        let viewController = UIViewController()
+        viewController.view.backgroundColor = .red
+        window?.rootViewController = viewController
+        window?.makeKeyAndVisible()
+    }
+
+    func sceneDidDisconnect(_ scene: UIScene) { }
+
+    func sceneDidBecomeActive(_ scene: UIScene) { }
+
+    func sceneWillResignActive(_ scene: UIScene) { }
+
+    func sceneWillEnterForeground(_ scene: UIScene) { }
+
+    func sceneDidEnterBackground(_ scene: UIScene) { }
+}
---
+++ Workspace.swift
@@ -0,0 +1,4 @@
+import ProjectDescription
+import DependencyPlugin
+
+let workspace = Workspace(name: "Lyrics", projects: ["Projects/*"])
