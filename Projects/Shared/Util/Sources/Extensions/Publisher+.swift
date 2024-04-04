953b18ec908d1ec4e30776a1ca3af3a7c9f6576a
derrickkim0109 <derrickkim0109@naver.com>
2024-04-04 23:15:27 +0900

    feat(Publisher+): Publisher+ 익스텐션 구현
    - HTTP Response Error를 위한 Publisher
    - Json decode를 위한 기능 구현

---
+++ Projects/Shared/Util/Sources/Extensions/Publisher+.swift
@@ -0,0 +1,80 @@
+//
+//  Publisher+.swift
+//  SharedUtil
+//
+//  Created by Derrick kim on 4/3/24.
+//
+
+import Foundation
+import Combine
+import SharedUtilInterface
+
+public typealias DataTaskResult = (data: Data, response: URLResponse)
+
+public extension Publisher where Output == DataTaskResult {
+    func validateStatusCode() -> AnyPublisher<DataTaskResult, NetworkError> {
+        return tryMap { data, response  in
+            guard let response = response as? HTTPURLResponse else {
+                throw NetworkError.noResponseError
+            }
+
+            switch response.statusCode {
+            case 200..<300:
+                return DataTaskResult(data : data, response: response)
+
+            case 400:
+                throw NetworkError.badRequestError
+
+            case 401:
+                throw NetworkError.authorizationError
+
+            case 403:
+                throw NetworkError.forbiddenError
+
+            case 404:
+                throw NetworkError.notFoundError
+
+            case 402, 405...499:
+                throw NetworkError.error4xx(response.statusCode)
+
+            case 500:
+                throw NetworkError.serverError
+
+            case 501...599:
+                throw NetworkError.error5xx(response.statusCode)
+
+            default:
+                throw NetworkError.unknownError
+            }
+        }
+        .mapError { error in
+            if let networkError = error as? NetworkError {
+                return networkError
+            } else {
+                return NetworkError.unknownError
+            }
+        }
+        .eraseToAnyPublisher()
+    }
+
+    func validateJsonValue<Output: Decodable>(to outputType: Output.Type) -> AnyPublisher<Output, NetworkError> {
+        return tryMap {
+            try JSONDecoder().decode(outputType, from: $0.data)
+        }
+        .mapError { error in
+            switch error {
+            case let decodingError as DecodingError:
+                return NetworkError.decodingError(decodingError.localizedDescription)
+            case let networkError as NetworkError:
+                return networkError
+            default:
+                return NetworkError.unknownError
+            }
+        }
+        .eraseToAnyPublisher()
+    }
+
+    func debug(prefix: String = "") -> Publishers.Print<Self> {
+        return print(prefix + "publisher", to: CombineTimeLogger())
+    }
+}
