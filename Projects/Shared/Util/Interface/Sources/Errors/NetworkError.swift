8c9be7d6ff4d7b3178bc7b97353c0cfe48c98272
derrickkim0109 <derrickkim0109@naver.com>
2024-04-04 23:13:02 +0900

    add(NetworkError): Network Error enum 타입 생성

---
+++ Projects/Shared/Util/Interface/Sources/Errors/NetworkError.swift
@@ -0,0 +1,76 @@
+//
+//  NetworkError.swift
+//  SharedUtilInterface
+//
+//  Created by Derrick kim on 4/4/24.
+//
+
+import Foundation
+
+public enum NetworkError: Error, Equatable {
+    case urlRequestError(URLRequestError)
+    case invalidURLError
+    case noResponseError
+    case forbiddenError
+    case notFoundError
+    case serverError
+    case error4xx(_ code: Int)
+    case error5xx(_ code: Int)
+    case decodingError( _ description: String)
+    case badRequestError
+    case authorizationError
+    case unknownError
+
+    public var errorMessage: String {
+        switch self {
+        case let .urlRequestError(urlRequestError):
+            return urlRequestError.errorMessage
+        case .decodingError:
+            return "Decoding Error"
+        case .invalidURLError:
+            return "Invalid URL"
+        case .noResponseError:
+            return "No Response"
+        case .forbiddenError:
+            return "Forbidden Error"
+        case .notFoundError:
+            return "Not Found Error"
+        case .serverError:
+            return "Server Error"
+        case .authorizationError:
+            return "Authorization Error"
+        case .badRequestError:
+            return "Bad Request From Client"
+        case .unknownError:
+            return "Unknown Error"
+        case .error4xx(let code):
+            return "error 4xx: \(code)"
+        case .error5xx(let code):
+            return "error 5xx: \(code)"
+        }
+    }
+
+    public enum URLRequestError: Error {
+        case urlComponentError
+        case queryEncodingError
+        case bodyEncodingError
+        case makeURLError
+
+        public var errorMessage: String {
+            switch self {
+            case .urlComponentError:
+                return "urlComponentError"
+            case .queryEncodingError:
+                return "queryEncodingError"
+            case .bodyEncodingError:
+                return "queryEncodingError"
+            case .makeURLError:
+                return "makeURLError"
+            }
+        }
+    }
+
+    public static func == (lhs: NetworkError, rhs: NetworkError) -> Bool {
+        return lhs.errorMessage == rhs.errorMessage
+    }
+}
