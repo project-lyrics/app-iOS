e48010e778326c435f077110c1e9a993ac77a193
derrickkim0109 <derrickkim0109@naver.com>
2024-04-04 23:11:07 +0900

    add(MockNetworkProvider): Mock Network Provider 생성

---
+++ Projects/Core/Network/Testing/Sources/MockNetworkProvider.swift
@@ -0,0 +1,45 @@
+//
+//  MockNetworkProvider.swift
+//  CoreNetwork
+//
+//  Created by Derrick kim on 4/3/24.
+//
+
+import Foundation
+import Combine
+import CoreNetworkInterface
+import SharedUtilInterface
+
+public final class MockNetworkProvider: NetworkProviderProtocol {
+    public var scenario: Scenario = .success
+
+    public var appSearchResponseDTO: AppSearchResponseDTO = {
+        let data = AppSearchItemResponseDTO.completeDataMock
+        
+        return AppSearchResponseDTO(
+            resultCount: 1,
+            results: [data]
+        )
+    }()
+
+    public var networkError = NetworkError.unknownError
+    
+    public func request<N, T>(
+        _ endpoint: N
+    ) -> AnyPublisher<T, NetworkError> where N: HTTPNetworking, T: Decodable, T == N.Response {
+        guard let data = appSearchResponseDTO as? T else {
+            return Fail(error: networkError)
+                .eraseToAnyPublisher()
+        }
+        
+        switch scenario {
+        case .success:
+            return Just(data)
+                .setFailureType(to: NetworkError.self)
+                .eraseToAnyPublisher()
+        case .failure:
+            return Fail(error: networkError)
+                .eraseToAnyPublisher()
+        }
+    }
+}
