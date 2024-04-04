06e27cad431bc85168e12ae0459348167d644640
derrickkim0109 <derrickkim0109@naver.com>
2024-04-04 23:08:37 +0900

    feat(NetworkProvider): NetworkProvider 기능 구현

---
+++ Projects/Core/Network/Sources/NetworkProvider.swift
@@ -0,0 +1,42 @@
+//
+//  NetworkProvider.swift
+//  CoreNetwork
+//
+//  Created by Derrick kim on 4/3/24.
+//
+
+import Foundation
+import CoreNetworkInterface
+import Combine
+import SharedUtilInterface
+
+public final class NetworkProvider: NetworkProviderProtocol {
+    private let session: URLSession
+
+    init(session: URLSession = URLSession.shared) {
+        self.session = session
+    }
+
+    public func request<N: HTTPNetworking, T: Decodable>(
+        _ endpoint: N
+    ) -> AnyPublisher<T, NetworkError> where N.Response == T {
+        do {
+            let urlRequest: URLRequest = try endpoint.makeURLRequest()
+
+            return session
+                .dataTaskPublisher(for: urlRequest)
+                .validateStatusCode()
+                .validateJsonValue(to: T.self)
+                .receive(on: DispatchQueue.main)
+                .eraseToAnyPublisher()
+        } catch let error {
+            if let networkError = error as? NetworkError {
+                return Fail(error: networkError)
+                    .eraseToAnyPublisher()
+            } else {
+                return Fail(error: NetworkError.unknownError)
+                    .eraseToAnyPublisher()
+            }
+        }
+    }
+}
