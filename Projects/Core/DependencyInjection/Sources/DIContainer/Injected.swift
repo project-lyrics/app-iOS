691a4cfa7ae0a3b3111b282a2e2f22817c2ae474
derrickkim0109 <derrickkim0109@naver.com>
2024-04-20 22:30:14 +0900

    feat<Injected>: Property Wrapper 구현

---
+++ Projects/Core/DependencyInjection/Sources/DIContainer/Injected.swift
@@ -0,0 +1,45 @@
+//
+//  Injected.swift
+//  Feature
+//
+//  Created by Derrick kim on 4/17/24.
+//
+
+import Foundation
+import CoreDependencyInjectionInterface
+
+@propertyWrapper public struct Injected<Value> {
+    public enum InjectError: Error {
+        case couldNotResolveAndIsNil
+    }
+
+    private let identifier: InjectIdentifier<Value>
+    private let container: Resolvable
+    private let `default`: Value?
+
+    public lazy var wrappedValue: Value = {
+        if let value = try? container.resolve(identifier) {
+            return value
+        }
+
+        if let `default` {
+            return `default`
+        }
+
+        fatalError("resolve failed with \(identifier) and default is nil as well.")
+    }()
+
+    public init(
+        _ identifier: InjectIdentifier<Value>? = nil,
+        container: Resolvable? = nil,
+        `default`: Value? = nil
+    ) {
+        self.identifier = identifier ?? .by(type: Value.self)
+        self.container = container ?? Self.container()
+        self.default = `default`
+    }
+
+    public static func container() -> Injectable {
+        return DIContainer.standard
+    }
+}
