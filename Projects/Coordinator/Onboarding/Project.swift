d924c9eb0035331c67a76f138c75500ad7e3ee71
derrickkim0109 <derrickkim0109@naver.com>
2024-05-10 17:29:49 +0900

    add<CoordinatorOnboarding>: Coordinator Onboarding 모듈 추가

---
+++ Projects/Coordinator/Onboarding/Project.swift
@@ -0,0 +1,38 @@
+import ProjectDescription
+import ProjectDescriptionHelpers
+import DependencyPlugin
+
+let project = Project.makeModule(
+    name: ModulePath.Coordinator.name + ModulePath.Coordinator.Onboarding.rawValue,
+    targets: [
+        .coordinator(
+            interface: .Onboarding,
+            factory: .init()
+        ),
+        .coordinator(
+            implements: .Onboarding,
+            factory: .init(
+                dependencies: [
+                    .coordinator(interface: .Onboarding)
+                ]
+            )
+        ),
+        .coordinator(
+            testing: .Onboarding,
+            factory: .init(
+                dependencies: [
+                    .coordinator(interface: .Onboarding)
+                ]
+            )
+        ),
+        .coordinator(
+            tests: .Onboarding,
+            factory: .init(
+                dependencies: [
+                    .coordinator(testing: .Onboarding)
+                ]
+            )
+        ),
+
+    ]
+)
