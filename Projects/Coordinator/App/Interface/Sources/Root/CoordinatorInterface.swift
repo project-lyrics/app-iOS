b95a70c71aef66779324b18b3016c7207fd3eb64
derrickkim0109 <derrickkim0109@naver.com>
2024-05-10 17:28:55 +0900

    feat<CoordinatorApp>: Coordinator, CoordinatorDelegate 프로포톨 구현

--- Projects/Coordinator/App/Interface/Sources/Root/CoordinatorInterface.swift
+++ Projects/Coordinator/App/Interface/Sources/Root/CoordinatorInterface.swift
@@ -6,3 +6,33 @@
 //
 
 import UIKit
+
+public protocol CoordinatorDelegate: AnyObject {
+    func didFinish(childCoordinator: Coordinator)
+}
+
+public protocol Coordinator: AnyObject {
+    var delegate: CoordinatorDelegate? { get set }
+    var navigationController: UINavigationController { get set }
+    var childCoordinators: [Coordinator] { get set }
+
+    func start()
+    func finish()
+    func popViewController()
+    func dismissViewController()
+}
+
+public extension Coordinator {
+    func finish() {
+        childCoordinators.removeAll()
+        delegate?.didFinish(childCoordinator: self)
+    }
+
+    func popViewController() {
+        navigationController.popViewController(animated: true)
+    }
+
+    func dismissViewController() {
+        navigationController.dismiss(animated: true)
+    }
+}
---
+++ Projects/Coordinator/Sources/CoordinatorExport.swift
@@ -0,0 +1,9 @@
+//
+//  DomainExport.swift
+//  Domain
+//
+//  Created by Derrick kim on 4/23/24.
+//
+
+@_exported import CoordinatorApp
+@_exported import CoordinatorAppInterface
