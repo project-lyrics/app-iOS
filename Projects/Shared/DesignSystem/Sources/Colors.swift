0c2985f372d4a370de9edc48289be2c3eda91086
derrickkim0109 <derrickkim0109@naver.com>
2024-05-16 23:25:42 +0900

    feat<Colors>: SharedDesignSystem 모듈 내의 Colors 타입 추가

--- Projects/Shared/DesignSystem/Sources/Colors.swift
+++ Projects/Shared/DesignSystem/Sources/Colors.swift
@@ -1,8 +1,36 @@
 //
-//  Images.swift
+//  Colors.swift
 //  SharedDesignSystem
 //
 //  Created by Derrickkim on 2024/02/18.
 //
 
-import Foundation
+import UIKit
+
+public struct Colors {
+    public static let active = SharedDesignSystemAsset.Colors.active.color
+    public static let alertSuccess = SharedDesignSystemAsset.Colors.alertSuccess.color
+    public static let alertWarning = SharedDesignSystemAsset.Colors.alertWarning.color
+    public static let background = SharedDesignSystemAsset.Colors.background.color
+    public static let border = SharedDesignSystemAsset.Colors.border.color
+    public static let dim = SharedDesignSystemAsset.Colors.dim.color
+    public static let disabled = SharedDesignSystemAsset.Colors.disabled.color
+    public static let gray01 = SharedDesignSystemAsset.Colors.gray01.color
+    public static let gray02 = SharedDesignSystemAsset.Colors.gray02.color
+    public static let gray03 = SharedDesignSystemAsset.Colors.gray03.color
+    public static let gray04 = SharedDesignSystemAsset.Colors.gray04.color
+    public static let gray05 = SharedDesignSystemAsset.Colors.gray05.color
+    public static let gray06 = SharedDesignSystemAsset.Colors.gray06.color
+    public static let gray07 = SharedDesignSystemAsset.Colors.gray07.color
+    public static let gray08 = SharedDesignSystemAsset.Colors.gray08.color
+    public static let gray09 = SharedDesignSystemAsset.Colors.gray09.color
+    public static let inputField = SharedDesignSystemAsset.Colors.inputField.color
+    public static let modal = SharedDesignSystemAsset.Colors.modal.color
+    public static let point = SharedDesignSystemAsset.Colors.point.color
+    public static let pressedGray = SharedDesignSystemAsset.Colors.pressedGray.color
+    public static let primary = SharedDesignSystemAsset.Colors.primary.color
+    public static let secondary = SharedDesignSystemAsset.Colors.secondary.color
+    public static let selectedBrand = SharedDesignSystemAsset.Colors.selectedBrand.color
+    public static let tertiary = SharedDesignSystemAsset.Colors.tertiary.color
+    public static let textFixed = SharedDesignSystemAsset.Colors.textFixed.color
+}
