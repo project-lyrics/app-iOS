6dac9c553be6fcdee0cb9448535372ed5ac1dcc7
derrickkim0109 <derrickkim0109@naver.com>
2024-05-17 11:19:18 +0900

    add<DesignSystem>: Onboarding에 필요한 Image Asset, Images 객체 추가
    - Images 객체는 Tuist에서 Asset의 이미지를 불러오기 위한 객체가 너무 길어서 만듬
      - 추가적으로 withRenderingMode를 정해줘야 함

--- Projects/Shared/DesignSystem/Sources/Images.swift
+++ Projects/Shared/DesignSystem/Sources/Images.swift
@@ -5,4 +5,39 @@
 //  Created by Derrickkim on 2024/02/18.
 //
 
-import Foundation
+import UIKit
+
+public struct Images {
+    public static let logo = SharedDesignSystemAsset.Images.logo.image
+    public static let backDark = SharedDesignSystemAsset.Images.backDark.image
+    public static let backLight = SharedDesignSystemAsset.Images.backLight.image
+    public static let caretDownDark = SharedDesignSystemAsset.Images.caretDownDark.image
+    public static let caretDownLight = SharedDesignSystemAsset.Images.caretDownLight.image
+    public static let checkBoxDarkInactive = SharedDesignSystemAsset.Images.checkBoxDarkInactive.image
+    public static let checkBoxDarkActive = SharedDesignSystemAsset.Images.checkBoxDarkActive.image
+    public static let checkBoxLightInactive = SharedDesignSystemAsset.Images.checkBoxLightInactive.image
+    public static let checkBoxLightActive = SharedDesignSystemAsset.Images.checkBoxLightActive.image
+    public static let notificationDarkOff = SharedDesignSystemAsset.Images.notificationDarkOff.image
+    public static let notificationDarkOn = SharedDesignSystemAsset.Images.notificationDarkOn.image
+    public static let notificationLightOff = SharedDesignSystemAsset.Images.notificationLightOff.image
+    public static let notificationLightOn = SharedDesignSystemAsset.Images.notificationLightOn.image
+    public static let pencil = SharedDesignSystemAsset.Images.pencil.image
+    public static let searchDark = SharedDesignSystemAsset.Images.searchDark.image
+    public static let searchLight = SharedDesignSystemAsset.Images.searchLight.image
+    public static let trashDark = SharedDesignSystemAsset.Images.trashDark.image
+    public static let trashLight = SharedDesignSystemAsset.Images.trashLight.image
+    public static let writingDark = SharedDesignSystemAsset.Images.writingDark.image
+    public static let writingLight = SharedDesignSystemAsset.Images.writingLight.image
+    public static let xCircleDark = SharedDesignSystemAsset.Images.xCircleDark.image
+    public static let xCircleLight = SharedDesignSystemAsset.Images.xCircleLight.image
+    public static let xDark = SharedDesignSystemAsset.Images.xDark.image
+    public static let xLight = SharedDesignSystemAsset.Images.xLight.image
+    public static let femaleActive = SharedDesignSystemAsset.Images.femaleActive.image
+    public static let femaleInactive = SharedDesignSystemAsset.Images.femaleInactive.image
+    public static let maleActive = SharedDesignSystemAsset.Images.maleActive.image
+    public static let maleInactive = SharedDesignSystemAsset.Images.maleInactive.image
+    public static let profile5To5PartedHair = SharedDesignSystemAsset.Images.profile5To5PartedHair.image
+    public static let profileBraidedHair = SharedDesignSystemAsset.Images.profileBraidedHair.image
+    public static let profilePoopHair = SharedDesignSystemAsset.Images.profilePoopHair.image
+    public static let profileShortHair = SharedDesignSystemAsset.Images.profileShortHair.image
+}
