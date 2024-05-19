e30d5b3f28514ad31b2df6fbca338c24fc5827bb
Inwoo Hwang <dlsdn1207@gmail.com>
2024-05-19 14:46:44 +0900

    chore: tuist의 모듈 세팅 중 LocalStorage name 수정

--- Projects/Core/LocalStorage/Project.swift
+++ Projects/Core/LocalStorage/Project.swift
@@ -3,7 +3,7 @@ import ProjectDescriptionHelpers
 import DependencyPlugin
 
 let project = Project.makeModule(
-    name: ModulePath.Core.name+ModulePath.Core.LocalStorage.rawValue,
+    name: ModulePath.Core.LocalStorage.rawValue,
     targets: [
         .core(
             interface: .LocalStorage,
