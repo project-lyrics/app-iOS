36f61e77ef184f718a4f8ea20cae9b35553c9cad
Inwoo Hwang <dlsdn1207@gmail.com>
2024-05-24 00:07:46 +0900

    feat : 카카오 네이티브 키를 관리할 xcconfig를 분리하여 관리
    - 본래는 Shared.xcconfig를 앱키 없이 올린 뒤 .gitignore설정을 하려 하였으나, 그렇게 되면 Shared자체에 추후 추가될 설정들 또한 다른 개발자들에게 공유가 되지 않을 것이기에 잘못된 방향이라고 생각함.
    - KakaoSecretKey라는 xcconfig를 생성하여 해당 설정을 통해서만 네이티브 앱키를 관리하는 방향으로 환경설정 수정
    - KakaoSecretKey 파일을 올린 뒤 이후 .gitignore처리를 통해 네이티브 앱 키 유출을 방지할 예정

--- Projects/App/Project.swift
+++ Projects/App/Project.swift
@@ -44,9 +44,12 @@ let appTargets: [Target] = [
 ]
 
 let appProject: Project = .makeModule(
-    name: "Feelin",
-    packages: [
-        .remote(url: "https://github.com/layoutBox/FlexLayout", requirement: .upToNextMajor(from: "2.0.7"))
-    ],
-    targets: appTargets
+    name: Project.Environment.appName,
+    settings: Project.Environment.projectSettings,
+    targets: appTargets,
+    schemes: appSchemes,
+    additionalFiles: [
+        "./xcconfigs/Shared.xcconfig",
+        "./xcconfigs/KakaoSecretKey.xcconfig"
+    ]
 )
