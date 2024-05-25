b5592f8e71065bb470e8cc56a5d0304e29ebfb03
derrickkim0109 <derrickkim0109@naver.com>
2024-05-25 16:05:59 +0900

    refactor<TargetModuleSetting>:
    - ThirdPartyLib dynamic Framework로 변경
    - 개행 추가
    - interface 위치변경
    - factory 마다 settings 추가 (FlexLayout을 위한 셋팅)

--- Plugins/DependencyPlugin/ProjectDescriptionHelpers/Project+Environment.swift
+++ Plugins/DependencyPlugin/ProjectDescriptionHelpers/Project+Environment.swift
@@ -12,7 +12,8 @@ public extension Project {
         public static let appName = "Feelin"
         public static let deploymentTargets = DeploymentTargets.iOS("15.0")
         public static let bundleId = "com.project.feelin"
-        public static let defaultSettings: Settings = .settings(
+        public static let appDefaultSettings: Settings = .settings(
+            base: ["GCC_PREPROCESSOR_DEFINITIONS": "$(inherited) FLEXLAYOUT_SWIFT_PACKAGE=1"],
             configurations: [
                 .debug(
                     name: "Debug",
@@ -24,6 +25,24 @@ public extension Project {
                     settings: [:],
                     xcconfig: nil
                 ),
+            ]
+        )
+        public static let defaultSettings: Settings = .settings(
+            base: [
+                "GCC_PREPROCESSOR_DEFINITIONS": "$(inherited) FLEXLAYOUT_SWIFT_PACKAGE=1",
+                "OTHER_LDFLAGS": "$(inherited) -all_load"
+            ]
+        )
+        public static let testsAppDefaultSettings: Settings = .settings(
+            base: [
+                "TEST_HOST": "",
+                "BUNDLE_LOADER": "$(BUILT_PRODUCTS_DIR)/$(TEST_TARGET_NAME).app/$(TEST_TARGET_NAME)"
+            ]
+        )
+        public static let exampleAppDefaultSettings: Settings = .settings(
+            base: [
+                "OTHER_LDFLAGS": "$(inherited) -Xlinker -interposable -all_load",
+                "GCC_PREPROCESSOR_DEFINITIONS": "$(inherited) FLEXLAYOUT_SWIFT_PACKAGE=1"
             ]
         )
     }
