7df550763259ae82a8199f524ab589f74e95e4ea
Inwoo Hwang <dlsdn1207@gmail.com>
2024-06-03 02:39:13 +0900

    feat<AppleOAuthService>: 애플 로그인 관련 코어 로직 객체 구현
    AppleLoginService모델을 활용하여 애플로그인을 통해 받아온 identityToken을 서버로 전송한 뒤 받은 AccessToken & RefreshToken을 활용하여
    로그인 여부를 알려주는 서비스 객체 구현

---
+++ Projects/Domain/OAuth/Interface/Sources/Service/AppleOAuthServiceInterface.swift
@@ -0,0 +1,29 @@
+//
+//  AppleOAuthServiceInterface.swift
+//  DomainOAuthInterface
+//
+//  Created by 황인우 on 6/3/24.
+//
+
+import Combine
+import CoreLocalStorageInterface
+import CoreNetworkInterface
+import Foundation
+
+public class AppleLoginService: NSObject {
+    // MARK: - 추후 accessTokenKey와 refreshTokenKey는 숨겨야 합니다.
+    public let accessTokenKey: String = "FeelinAccessTokenKey"
+    public let refreshTokenKey: String = "FeelinRefreshTokenKey"
+    public var networkProvider: NetworkProviderProtocol
+    public var tokenStorage: TokenStorageInterface
+    public let jwtDecoder: JWTDecoder = .init()
+    public var appleTokenSubject: PassthroughSubject<String, AppleOAuthError> = .init()
+    
+    public init(
+        networkProvider: NetworkProviderProtocol,
+        tokenStorage: TokenStorageInterface
+    ) {
+        self.networkProvider = networkProvider
+        self.tokenStorage = tokenStorage
+    }
+}
---
+++ Projects/Domain/OAuth/Sources/Service/AppleOAuthService.swift
@@ -0,0 +1,69 @@
+//
+//  AppleOAuthService.swift
+//  DomainOAuthInterface
+//
+//  Created by 황인우 on 6/3/24.
+//
+
+import AuthenticationServices
+import Combine
+import CoreNetworkInterface
+import Foundation
+import DomainOAuthInterface
+
+
+extension AppleLoginService: OAuthServiceInterface, UserVerifiable {
+    public func login() -> AnyPublisher<OAuthResult, AuthError> {
+        let request = ASAuthorizationAppleIDProvider().createRequest()
+        request.requestedScopes = [.fullName, .email]
+
+        let controller = ASAuthorizationController(authorizationRequests: [request])
+        controller.delegate = self
+        controller.presentationContextProvider = self
+        controller.performRequests()
+
+        return appleTokenSubject
+            .eraseToAnyPublisher()
+            .mapError(AuthError.appleOAuthError)
+            .map { ($0, OAuthProvider.apple) }
+            .flatMap(verifyUser)
+            .eraseToAnyPublisher()
+    }
+}
+
+extension AppleLoginService: ASAuthorizationControllerDelegate {
+    public func authorizationController(controller: ASAuthorizationController, didCompleteWithAuthorization authorization: ASAuthorization) {
+        guard let appleIDCredential = authorization.credential as? ASAuthorizationAppleIDCredential else {
+            appleTokenSubject.send(completion: .failure(.unknown))
+            return
+        }
+        
+        guard let identityTokenData = appleIDCredential.identityToken else {
+            appleTokenSubject.send(completion: .failure(.tokenMissing))
+            return
+        }
+        
+        let identityToken =  String(
+            decoding: identityTokenData,
+            as: UTF8.self
+        )
+        
+        self.appleTokenSubject.send(identityToken)
+    }
+    
+    public func authorizationController(controller: ASAuthorizationController, didCompleteWithError error: Error) {
+        appleTokenSubject.send(
+            completion: .failure(.init(error: error))
+        )
+    }
+}
+
+extension AppleLoginService: ASAuthorizationControllerPresentationContextProviding {
+    public func presentationAnchor(for controller: ASAuthorizationController) -> ASPresentationAnchor {
+        guard let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene,
+              let window = windowScene.windows.first(where: { $0.isKeyWindow }) else {
+            return UIWindow()
+        }
+        return window
+    }
+}
