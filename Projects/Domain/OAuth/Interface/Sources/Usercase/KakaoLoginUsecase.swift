a5a1dee3993138baa43c99cb9bec66d1bbd73922
derrickkim0109 <derrickkim0109@naver.com>
2024-06-08 01:48:23 +0900

    style<DomainOAuth>: UseCase로 네이밍 변경

--- Projects/Domain/OAuth/Interface/Sources/Usercase/KakaoLoginUsecase.swift
+++ Projects/Domain/OAuth/Interface/Sources/Usercase/KakaoLoginUsecase.swift
@@ -1,5 +1,5 @@
 //
-//  KakaoLoginUsecase.swift
+//  KakaoLoginUseCase.swift
 //  DomainOAuthInterface
 //
 //  Created by 황인우 on 5/28/24.
@@ -8,14 +8,14 @@
 import Combine
 import Foundation
 
-public struct KakaoLoginUsecase {
+public struct KakaoLoginUseCase {
     private let kakaoOAuthService: KakaoOAuthServiceInterface
     
     public init(kakaoOAuthService: KakaoOAuthServiceInterface) {
         self.kakaoOAuthService = kakaoOAuthService
     }
     
-    func execute() -> AnyPublisher<OAuthResult, AuthError> {
+    public func execute() -> AnyPublisher<OAuthResult, AuthError> {
         return kakaoOAuthService.login()
     }
 }
