a0f1387f9d61af884271975189b104f647938b56
Inwoo Hwang <dlsdn1207@gmail.com>
2024-06-09 22:14:51 +0900

    feat<공용컴포넌트> FeelinConfirmButton 제작
    - 비활성화시 회색, 활성화시 active 색상으로 변경되는 버튼 구현
    - 필요시 버튼 타이틀, 그리고 활성화 상태 뿐 아니라 각 상태에 따른 색상을 외부에서 지정할 수 있도록 세팅

---
+++ Projects/Shared/DesignSystem/Sources/Buttons/FeelinConfirmButton.swift
@@ -0,0 +1,54 @@
+//
+//  FeelinConfirmButton.swift
+//  SharedDesignSystem
+//
+//  Created by 황인우 on 6/9/24.
+//
+
+import UIKit
+
+public class FeelinConfirmButton: UIButton {
+    private var enabledBackgroundColor = Colors.active
+    private var disabledBackgroundColor = Colors.disabled
+    
+    public init(
+        initialEnabled: Bool = false,
+        title: String,
+        enabledBackgroundColor: UIColor = Colors.active,
+        disabledBackgroundColor: UIColor = Colors.disabled
+    ) {
+        super.init(frame: .zero)
+        self.isEnabled = initialEnabled
+        self.enabledBackgroundColor = enabledBackgroundColor
+        self.disabledBackgroundColor = disabledBackgroundColor
+        setupButton(title: title)
+    }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+    private func setupButton(title: String) {
+        updateAppearance()
+        
+        self.setTitle(title, for: .normal)
+        self.titleLabel?.font = UIFont.pretendard(size: 16, type: .semiBold)
+    }
+    
+    private func updateAppearance() {
+        if isEnabled {
+            backgroundColor = enabledBackgroundColor
+            setTitleColor(.white, for: .normal)
+        } else {
+            backgroundColor = disabledBackgroundColor
+            setTitleColor(.white, for: .normal)
+        }
+    }
+    
+    public override var isEnabled: Bool {
+        didSet {
+            updateAppearance()
+        }
+    }
+}
