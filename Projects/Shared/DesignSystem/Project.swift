d38413d2efd3e518ef845ee43284e15580115ea4
noeyiz <lamoureclatant@gmail.com>
2024-06-14 02:06:23 +0900

    feat: FeelinDropDownButton 추가

--- Projects/Shared/DesignSystem/Project.swift
+++ Projects/Shared/DesignSystem/Project.swift
@@ -14,7 +14,8 @@ let targets: [Target] = [
         implements: .DesignSystem,
         factory: .init(
             dependencies: [
-                .shared(implements: .ThirdPartyLib)
+                .shared(implements: .ThirdPartyLib),
+                .SPM.FlexLayout
             ]
         )
     )
@@ -22,6 +23,9 @@ let targets: [Target] = [
 
 let project: Project = .makeModule(
     name: ModulePath.Shared.DesignSystem.rawValue,
+    packages: [
+        .remote(url: "https://github.com/layoutBox/FlexLayout", requirement: .upToNextMajor(from: "2.0.7"))
+    ],
     targets: targets,
     resourceSynthesizers: [
         .assets(),
