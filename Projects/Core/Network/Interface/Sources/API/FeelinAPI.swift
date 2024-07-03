a14fcb0dca9d765aefe21a349cd352e690ea2672
derrickkim0109 <derrickkim0109@naver.com>
2024-07-03 22:34:48 +0900

    refactor: FeelinAPI URL, path 변경

--- Projects/Core/Network/Interface/Sources/API/FeelinAPI.swift
+++ Projects/Core/Network/Interface/Sources/API/FeelinAPI.swift
@@ -20,41 +20,43 @@ public enum FeelinAPI<R> {
 extension FeelinAPI: HTTPNetworking {
     public var headers: [String : String]? {
         var defaultHeader = ["application/json": "Content-Type"]
-        
+
         switch self {
         case .login(_, oauthAccessToken: let oAuthAccessToken):
             defaultHeader["Authorization"] = "Bearer \(oAuthAccessToken)"
-            
+
         case .reissueAccessToken(refreshToken: let refreshToken):
             defaultHeader["Authorization"] = "Bearer \(refreshToken)"
-        
+
         default:
             return defaultHeader
         }
-        
+
         return defaultHeader
     }
-    
+
     public var queryParameters: Encodable? {
         return nil
     }
-    
+
     public var bodyParameters: Encodable? {
         switch self {
-        case .login(let oauthProvider, _):
-            return ["auth_provider": oauthProvider.rawValue]
-        
+        case .login(let oauthProvider, let oauthAccessToken):
+            return [
+                "socialAccessToken": oauthAccessToken,
+                "authProvider": oauthProvider.rawValue
+            ]
+
         default:
             return nil
         }
     }
-    
+
     public typealias Response = R
 
     public var baseURL: String? {
         guard let baseURL = Bundle.main.infoDictionary?["Feelin_URL"] as? String else {
-            // MARK: - 추후 임시 도메인주소라도 넣어줘야 한다. 테스트하기 위해 nil 대신 일단 임시 설정
-            return "www.feelin.com"
+            return "http://api.feelinapp.com:8080"
         }
 
         return baseURL
@@ -63,11 +65,11 @@ extension FeelinAPI: HTTPNetworking {
     public var path: String {
         switch self {
         case .login:
-            return "/api/v1/auth/login"
-            
+            return "/api/v1/auth/sign-in"
+
         case .checkUserValidity:
             return "/api/v1/auth/token"
-            
+
         case .reissueAccessToken:
             return "/api/v1/auth/token"
         }
@@ -77,7 +79,7 @@ extension FeelinAPI: HTTPNetworking {
         switch self {
         case .login, .reissueAccessToken:
             return .post
-        
+
         case .checkUserValidity:
             return .get
         }
