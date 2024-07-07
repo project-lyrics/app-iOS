a2b32fc8dfff4873dafa8ba0dda2d5aae0448bae
derrickkim0109 <derrickkim0109@naver.com>
2024-07-07 23:56:50 +0900

    feat: Sign Up을 위한 Entity Model 구현

--- Projects/Feature/Onboarding/Interface/Sources/Models/GenderEntity.swift
+++ Projects/Feature/Onboarding/Interface/Sources/Models/GenderEntity.swift
@@ -1,32 +1,29 @@
 //
-//  GenderType.swift
+//  GenderEntity.swift
 //  FeatureOnboardingInterface
 //
 //  Created by jiyeon on 6/12/24.
 //
 
 import UIKit
-
 import SharedDesignSystem
+import Domain
 
-enum GenderType: CaseIterable {
-    case male
-    case female
-    
+extension GenderEntity {
     var description: String {
         switch self {
         case .male:     "남성"
         case .female:   "여성"
         }
     }
-    
+
     var activeImage: UIImage {
         switch self {
         case .male:     FeelinImages.maleActive
         case .female:   FeelinImages.femaleActive
         }
     }
-    
+
     var inactiveImage: UIImage {
         switch self {
         case .male:     FeelinImages.maleInactive
