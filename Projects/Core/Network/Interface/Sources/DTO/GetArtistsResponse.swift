b09976c885e94baa6be4c2fa4b117b8718a89c61
Inwoo Hwang <dlsdn1207@gmail.com>
2024-07-21 20:24:38 +0900

    feat, refactor: 좋아하는 아티스트 화면 대응 FeelinAPI 수정 및 관련 Response 객체 구현
    - FeelinAPI
      - getArtist, searchArtist, postFavoriteArtists api 추가
    
    - Response
      - GetArtistResponse & EmptyResponse 추가

---
+++ Projects/Core/Network/Interface/Sources/DTO/GetArtistsResponse.swift
@@ -0,0 +1,14 @@
+//
+//  GetArtistsResponse.swift
+//  CoreNetworkInterface
+//
+//  Created by 황인우 on 7/14/24.
+//
+
+import Foundation
+
+public struct GetArtistsResponse: Decodable {
+    public let nextCursor: Int?
+    public let hasNext: Bool
+    public let data: [ArtistResponse]
+}
