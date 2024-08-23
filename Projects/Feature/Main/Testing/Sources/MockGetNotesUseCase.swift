ab5db26391b534a696aae22519bdf271415c5fe6
Inwoo Hwang <dlsdn1207@gmail.com>
2024-08-24 02:53:04 +0900

    add : MainView & MainViewController를 example 모듈에서 확인할 수 있는 세팅
    Mock객체들 그리고 Preview를 통해서 네트워크가 없는 환경에서도 홈화면의 UI구현 진행내역을 바로바로 확인할 수 있는 세팅 적용

---
+++ Projects/Feature/Main/Testing/Sources/MockGetNotesUseCase.swift
@@ -0,0 +1,118 @@
+//
+//  MockGetNotesUseCase.swift
+//  FeatureMainTesting
+//
+//  Created by 황인우 on 8/18/24.
+//
+
+import Combine
+import Foundation
+
+import Domain
+
+public struct MockGetNotesUseCase: GetNotesUseCaseInterface {
+    public init() {}
+    
+    public func execute(
+        isInitial: Bool,
+        perPage: Int,
+        mustHaveLyrics: Bool
+    ) -> AnyPublisher<[Note], NoteError> {
+        return Just(
+            [
+                Note(
+                    id: 1,
+                    content: "테스트 내용입니다.",
+                    status: .published,
+                    createdAt: .distantPast,
+                    lyrics: .init(
+                        content: "난 너의 아픔 속에 너의 고통 속에 기생\n하고 있는 아름다운 벌레야",
+                        background: .skyblue
+                    ),
+                    publisher: .init(
+                        id: 1,
+                        nickname: "테스트유저1",
+                        profileCharacterType: .poopHair
+                    ),
+                    song: .init(
+                        id: 1,
+                        name: "테스트 노래",
+                        imageUrl: "1234.com",
+                        artist: .init(
+                            id: 1,
+                            name: "테스트 아티스트",
+                            imageSource: "www.com",
+                            isFavorite: false
+                        )
+                    ),
+                    commentsCount: 0,
+                    likesCount: 0,
+                    isLiked: false,
+                    isBookmarked: false
+                ),
+                Note(
+                    id: 2,
+                    content: "테스트 내용입니다2.",
+                    status: .published,
+                    createdAt: .distantPast,
+                    lyrics: .init(
+                        content: "난 너의 아픔 속에 너의 고통 속에 기생\n하고 있는 아름다운 벌레야\n 이 밤이 지나고 우린 이상하지 않을까. 우린 어디서부터 잘못된 걸까 알고 싶지만 이제 더 이상 미련도 갖지 않은 내 자신이 이상해",
+                        background: .black
+                    ),
+                    publisher: .init(
+                        id: 1,
+                        nickname: "테스트유저2",
+                        profileCharacterType: .poopHair
+                    ),
+                    song: .init(
+                        id: 1,
+                        name: "테스트 노래2",
+                        imageUrl: "1234.com",
+                        artist: .init(
+                            id: 1,
+                            name: "테스트 아티스트2",
+                            imageSource: "www.com",
+                            isFavorite: false
+                        )
+                    ),
+                    commentsCount: 0,
+                    likesCount: 0,
+                    isLiked: false,
+                    isBookmarked: false
+                ),
+                Note(
+                    id: 3,
+                    content: "테스트 내용입니다3.",
+                    status: .published,
+                    createdAt: .distantPast,
+                    lyrics: .init(
+                        content: "난 너의 아픔 속에 너의 고통",
+                        background: .beige
+                    ),
+                    publisher: .init(
+                        id: 1,
+                        nickname: "테스트유저3",
+                        profileCharacterType: .poopHair
+                    ),
+                    song: .init(
+                        id: 1,
+                        name: "테스트 노래3",
+                        imageUrl: "1234.com",
+                        artist: .init(
+                            id: 1,
+                            name: "테스트 아티스트3",
+                            imageSource: "www.com",
+                            isFavorite: false
+                        )
+                    ),
+                    commentsCount: 0,
+                    likesCount: 0,
+                    isLiked: false,
+                    isBookmarked: false
+                )
+            ]
+        )
+        .setFailureType(to: NoteError.self)
+        .eraseToAnyPublisher()
+    }
+}
