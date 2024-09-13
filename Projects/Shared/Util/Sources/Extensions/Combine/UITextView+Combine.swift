0c693ceadb78d20aa2f6fe899a5924c1865d6c48
derrickkim0109 <derrickkim0109@naver.com>
2024-09-13 23:47:50 +0900

    feat: UITextView shouldBeginEditingSubject Combine 기능 구현

--- Projects/Shared/Util/Sources/Extensions/Combine/UITextView+Combine.swift
+++ Projects/Shared/Util/Sources/Extensions/Combine/UITextView+Combine.swift
@@ -9,6 +9,33 @@ import Combine
 import UIKit
 
 public extension UITextView {
+    private class TextViewDelegate: NSObject, UITextViewDelegate {
+        let shouldBeginEditingSubject = PassthroughSubject<UITextView, Never>()
+        var allowEditingPublisher: AnyPublisher<Bool, Never> = Just(true).eraseToAnyPublisher()
+        private var cancellable = Set<AnyCancellable>()
+
+        func textViewShouldBeginEditing(_ textView: UITextView) -> Bool {
+            shouldBeginEditingSubject.send(textView)
+
+            var allowEditing = true
+            let semaphore = DispatchSemaphore(value: 0)
+
+            allowEditingPublisher
+                .sink { isAllowed in
+                    allowEditing = isAllowed
+                    semaphore.signal()
+                }
+                .store(in: &cancellable)
+
+            semaphore.wait()
+            return allowEditing
+        }
+    }
+
+    private struct AssociatedKeys {
+        static var delegateKey = "textViewDelegateKey"
+    }
+
     enum TextViewEvent {
         case didBeginEditing
         case didEndEditing
@@ -36,8 +63,25 @@ public extension UITextView {
             .eraseToAnyPublisher()
     }
 
+    var shouldBeginEditingPublisher: AnyPublisher<UITextView, Never> {
+        if let delegate = objc_getAssociatedObject(self, AssociatedKeys.delegateKey) as? TextViewDelegate {
+            return delegate.shouldBeginEditingSubject.eraseToAnyPublisher()
+        } else {
+            let delegate = TextViewDelegate()
+            self.delegate = delegate
+            objc_setAssociatedObject(self, AssociatedKeys.delegateKey, delegate, .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
+            return delegate.shouldBeginEditingSubject.eraseToAnyPublisher()
+        }
+    }
+
     func setUpTextView(text: String, textColor: UIColor) {
         self.text = text
         self.textColor = textColor
+    }
+
+    func setAllowEditingPublisher(_ publisher: AnyPublisher<Bool, Never>) {
+        if let delegate = objc_getAssociatedObject(self, AssociatedKeys.delegateKey) as? TextViewDelegate {
+            delegate.allowEditingPublisher = publisher
+        }
     }
 }
