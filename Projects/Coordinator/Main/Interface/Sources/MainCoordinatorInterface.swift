09541f9d848f0d4f3de86354ce0486ef521f9861
derrickkim0109 <derrickkim0109@naver.com>
2024-09-13 23:54:15 +0900

    feat: MainCoordinatorInterface PostNote ViewController 추가

--- Projects/Coordinator/Main/Interface/Sources/MainCoordinatorInterface.swift
+++ Projects/Coordinator/Main/Interface/Sources/MainCoordinatorInterface.swift
@@ -1,6 +1,9 @@
 import UIKit
 import CoordinatorAppInterface
 import FeatureMainInterface
+import DependencyInjection
+import Domain
+import Core
 
 public final class MainCoordinator: Coordinator {
     public weak var delegate: CoordinatorDelegate?
@@ -13,30 +16,72 @@ public final class MainCoordinator: Coordinator {
     }
 
     public func start() {
-        configureMainController()
+        registerNetwork()
     }
 }
 
 private extension MainCoordinator {
     func configureMainController() {
-        
+
+    }
+
+    func registerNetwork() {
+        DIContainer.registerNetworkProvider(hasTokenStorage: true)
+    }
+
+    func registerPostNoteDI() {
+        DIContainer.registerDependenciesForPostNote()
+    }
+
+    func postNoteDependencies(artistID: Int) -> PostNoteViewModel {
+        @Injected(.noteService) var noteService: NoteServiceInterface
+        let postNoteUseCase: PostNoteUseCaseInterface = PostNoteUseCase(noteService: noteService)
+        let viewModel = PostNoteViewModel(postNoteUseCase: postNoteUseCase, artistID: artistID)
+
+        return viewModel
     }
 }
 
-extension MainCoordinator: CoordinatorDelegate, 
+extension MainCoordinator: CoordinatorDelegate,
                            PostNoteViewControllerDelegate,
                            SearchSongViewControllerDelegate {
-    public func didFinish(id songID: Int) {
-
+    public func didFinish(selectedItem: DomainSharedInterface.Song) {
+        if let postNoteViewController = navigationController.viewControllers.first(where: { $0 is PostNoteViewController }) as? PostNoteViewController {
+            postNoteViewController.addSelectedSong(selectedItem)
+            popViewController()
+        }
     }
 
     public func didFinish() {
 
     }
 
-    public func pushSearchSongViewController() {
-        let viewController = SearchSongViewController()
-        navigationController.pushViewController(viewController, animated: true)
+    public func pushPostNoteViewController(artistID: Int) {
+        registerPostNoteDI()
+        let viewModel = postNoteDependencies(artistID: artistID)
+        let postNoteViewController = PostNoteViewController(viewModel: viewModel)
+        postNoteViewController.coordinator = self
+        navigationController.pushViewController(postNoteViewController, animated: true)
+    }
+
+    public func pushSearchSongViewController(artistID: Int) {
+        @Injected(.noteService) var noteService: NoteServiceInterface
+        @Injected(.songPaginationService) var songPaginationService: SongPaginationServiceInterface
+
+        songPaginationService.resetPagination()
+
+        let searchSongUseCase = SearchSongUseCase(
+            noteService: noteService,
+            songPaginationService: songPaginationService
+        )
+        let searchSongViewModel = SearchSongViewModel(
+            searchSongUseCase: searchSongUseCase,
+            artistID: artistID
+        )
+
+        let searchSongViewController = SearchSongViewController(viewModel: searchSongViewModel)
+        searchSongViewController.coordinator = self
+        navigationController.pushViewController(searchSongViewController, animated: true)
     }
 
     public func didFinish(childCoordinator: Coordinator) {
