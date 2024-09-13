c68bee7012bc3828d2a4f59d631860292fd1c884
derrickkim0109 <derrickkim0109@naver.com>
2024-09-14 00:24:48 +0900

    feat: PostNote ViewController 및 ViewModel 구현

--- Projects/Feature/Main/Interface/Sources/ViewModels/PostNoteViewModel.swift
+++ Projects/Feature/Main/Interface/Sources/ViewModels/PostNoteViewModel.swift
@@ -5,21 +5,143 @@
 //  Created by Derrick kim on 8/7/24.
 //
 
-import Foundation
+import UIKit
+import Combine
+
+import Domain
 
 public final class PostNoteViewModel {
     struct Input {
-
+        let songTapPublisher: AnyPublisher<Song, Never>
+        let lyricsTextViewTypePublisher: AnyPublisher<String?, Never>
+        let lyricsBackgroundSelectPublisher: AnyPublisher<LyricsBackground?, Never>
+        let noteTextViewTypePublisher: AnyPublisher<String, Never>
+        let completeButtonTapPublisher: AnyPublisher<UIControl, Never>
+        let postNoteStatusPublisher: AnyPublisher<NoteStatus, Never>
     }
+
     struct Output {
-
+        let isEnabledCompleteButton: AnyPublisher<Bool, Never>
+        let isEnabledLyricsBackgroundButton: AnyPublisher<Bool, Never>
+        let isSelectedSong: AnyPublisher<Bool, Never>
+        let isSelectedLyricsBackground: AnyPublisher<LyricsBackground?, Never>
+        let postNoteResult: AnyPublisher<NoteResult, Never>
     }
 
-    public init() {
+    private var cancellables = Set<AnyCancellable>()
+    private var error: SongsError?
+    private let postNoteUseCase: PostNoteUseCaseInterface
 
+    public let artistID: Int
+
+    public init(
+        postNoteUseCase: PostNoteUseCaseInterface,
+        artistID: Int
+    ) {
+        self.postNoteUseCase = postNoteUseCase
+        self.artistID = artistID
     }
 
     func transform(input: Input) -> Output {
-        return Output()
+        return Output(
+            isEnabledCompleteButton: self.isEnabledCompleteButton(input),
+            isEnabledLyricsBackgroundButton: self.checkLyricsText(input),
+            isSelectedSong: self.isSelectedSong(input), isSelectedLyricsBackground: self.isSelectLyricsBackground(input),
+            postNoteResult: self.postNote(input)
+        )
+    }
+}
+
+private extension PostNoteViewModel {
+    func isEnabledCompleteButton(_ input: Input) -> AnyPublisher<Bool, Never> {
+        return Publishers.CombineLatest3(
+            input.songTapPublisher,
+            input.noteTextViewTypePublisher,
+            input.postNoteStatusPublisher
+        )
+        .map { song, noteContent, status in
+            return !noteContent.isEmpty && status == .draft
+        }
+        .eraseToAnyPublisher()
+    }
+
+    func isSelectedSong(_ input: Input) -> AnyPublisher<Bool, Never> {
+        return input.songTapPublisher
+            .map { song in
+                return song.name.isEmpty == false
+            }
+            .eraseToAnyPublisher()
+    }
+
+    func isSelectLyricsBackground(_ input: Input) -> AnyPublisher<LyricsBackground?, Never> {
+        return input.lyricsBackgroundSelectPublisher
+            .map { background in
+                return background
+            }
+            .eraseToAnyPublisher()
+    }
+
+    func checkLyricsText(_ input: Input) -> AnyPublisher<Bool, Never> {
+        return input.lyricsTextViewTypePublisher
+            .map { text in
+                return text?.isEmpty == false
+            }
+            .eraseToAnyPublisher()
+    }
+
+    func postNote(_ input: Input) -> AnyPublisher<NoteResult, Never> {
+        let requiredFieldsPublisher = Publishers.CombineLatest3(
+            input.songTapPublisher,
+            input.noteTextViewTypePublisher,
+            input.postNoteStatusPublisher
+        )
+        .eraseToAnyPublisher()
+
+        let optionalFieldsPublisher = Publishers.CombineLatest(
+            input.lyricsTextViewTypePublisher,
+            input.lyricsBackgroundSelectPublisher
+        )
+        .map { (lyrics, background) -> (String?, LyricsBackground?) in
+            return (lyrics, background)
+        }
+        .eraseToAnyPublisher()
+
+        // 필수 필드와 선택 필드를 결합
+        let combinedPublisher = requiredFieldsPublisher
+            .combineLatest(optionalFieldsPublisher)
+            .map { (requiredFields, optionalFields) -> PostNoteValue in
+                let (song, noteContent, status) = requiredFields
+                let (lyrics, lyricsBackground) = optionalFields
+
+                return PostNoteValue(
+                    id: song.id,
+                    lyrics: lyrics,
+                    background: lyricsBackground,
+                    content: noteContent,
+                    status: status
+                )
+            }
+            .eraseToAnyPublisher()
+
+        // 완료 버튼 탭과 결합된 필드로 노트를 게시
+        return input.completeButtonTapPublisher
+            .combineLatest(combinedPublisher)
+            .flatMap { (_, value) in
+                return self.postNote(value)
+            }
+            .eraseToAnyPublisher()
+
+    }
+
+    func postNote(_ requestValue: PostNoteValue) -> AnyPublisher<NoteResult, Never> {
+        return self.postNoteUseCase
+            .execute(value: requestValue)
+            .mapError(NoteError.init)
+            .receive(on: DispatchQueue.main)
+            .catch { error in
+                return Just(.failure(error))
+                    .eraseToAnyPublisher()
+            }
+            .eraseToAnyPublisher()
     }
 }
