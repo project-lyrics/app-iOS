3bef1e37a241283b094a46e44036ae24956ef0d8
derrickkim0109 <derrickkim0109@naver.com>
2024-10-01 09:08:35 +0900

    remove: Tuist Manifest에 불필요한 설정 제거

--- Projects/Feature/Onboarding/Project.swift
+++ Projects/Feature/Onboarding/Project.swift
@@ -57,8 +57,5 @@ let targets: [Target] = [
 
 let project = Project.makeModule(
     name: ModulePath.Feature.name + ModulePath.Feature.Onboarding.rawValue,
-    packages: [
-        .remote(url: "https://github.com/layoutBox/FlexLayout", requirement: .upToNextMajor(from: "2.0.7"))
-    ],
     targets: targets
 )
--- Projects/Feature/Project.swift
+++ Projects/Feature/Project.swift
@@ -23,8 +23,5 @@ let targets: [Target] = [
 
 let project: Project = .makeModule(
     name: "Feature",
-    packages: [
-        .remote(url: "https://github.com/layoutBox/FlexLayout", requirement: .upToNextMajor(from: "2.0.7"))
-    ],
     targets: targets
 )
