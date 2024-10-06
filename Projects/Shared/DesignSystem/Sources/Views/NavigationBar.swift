8f9e452ea168325b0ab512d999b97bdccba04ec7
derrickkim0109 <derrickkim0109@naver.com>
2024-10-06 19:07:16 +0900

    refactor: NavigationBar 간격 수정

--- Projects/Shared/DesignSystem/Sources/Views/NavigationBar.swift
+++ Projects/Shared/DesignSystem/Sources/Views/NavigationBar.swift
@@ -26,28 +26,46 @@ public final class NavigationBar: UIView {
 }
 
 public extension NavigationBar {
-    func addLeftBarView(_ contentView: UIView) {
+    func addLeftBarView(_ contentViews: [UIView]) {
+        let count = contentViews.count > rightBarView.subviews.count ? contentViews.count : rightBarView.subviews.count
+
         leftBarView.flex
+            .direction(.row)
+            .shrink(1)
             .define { flex in
-                flex.addItem(contentView)
+                contentViews.enumerated().forEach { index, contentView in
+                    flex.addItem(contentView)
+                        .marginRight(index < contentViews.count - 1 ? 20 : 0) // 마지막 아이템 오른쪽에는 마진 없음
+                }
             }
+        leftBarView.flex.width(CGFloat(count == 1 ? 44 : 68)) // 한 개일 때 44, 두 개일 때 96
         rootFlexContainer.flex.layout()
     }
-    
+
     func addTitleView(_ contentView: UIView) {
         titleView.flex
             .grow(1)
             .define { flex in
                 flex.addItem(contentView)
             }
+
+        contentView.flex.markDirty()
         rootFlexContainer.flex.layout()
     }
     
-    func addRightBarView(_ contentView: UIView) {
+    func addRightBarView(_ contentViews: [UIView]) {
+        let count = contentViews.count > leftBarView.subviews.count ? contentViews.count : leftBarView.subviews.count
+
         rightBarView.flex
+            .direction(.row)
+            .shrink(1)
             .define { flex in
-                flex.addItem(contentView)
+                contentViews.enumerated().forEach { index, contentView in
+                    flex.addItem(contentView)
+                        .marginRight(index < contentViews.count - 1 ? 20 : 0) // 마지막 아이템 오른쪽에는 마진 없음
+                }
             }
+        rightBarView.flex.width(CGFloat(count == 1 ? 44 : 68))
         rootFlexContainer.flex.layout()
     }
 }
@@ -63,16 +81,18 @@ private extension NavigationBar {
                 flex.addItem(leftBarView)
                     .justifyContent(.center)
                     .alignItems(.center)
-                    .size(24)
+                    .width(44)
+                    .height(100%)
                     .margin(
                         UIEdgeInsets(
                             top: 10,
                             left: 0,
                             bottom: 10,
-                            right: 8
+                            right: 0
                         )
                     )
-                
+                    .markDirty()
+
                 flex.addItem(titleView)
                     .justifyContent(.center)
                     .alignItems(.center)
@@ -81,24 +101,27 @@ private extension NavigationBar {
                     .margin(
                         UIEdgeInsets(
                             top: 10,
-                            left: 16,
-                            bottom: 10,
-                            right: 16
-                        )
-                    )
-                
-                flex.addItem(rightBarView)
-                    .justifyContent(.center)
-                    .alignItems(.center)
-                    .size(44)
-                    .margin(
-                        UIEdgeInsets(
-                            top: 5,
                             left: 0,
-                            bottom: 5,
+                            bottom: 10,
                             right: 0
                         )
                     )
+                    .markDirty()
+
+                flex.addItem(rightBarView)
+                    .justifyContent(.center)
+                    .alignItems(.center)
+                    .width(44)
+                    .height(100%)
+                    .margin(
+                        UIEdgeInsets(
+                            top: 10,
+                            left: 0,
+                            bottom: 10,
+                            right: 0
+                        )
+                    )
+                    .markDirty()
             }
     }
 }
@@ -108,16 +131,19 @@ private extension NavigationBar {
 public extension NavigationBar {
     func hideRightBarView(_ mustHide: Bool) {
         rightBarView.isHidden = mustHide
+        rightBarView.flex.markDirty()
         rootFlexContainer.flex.layout()
     }
     
     func hideTitleView(_ mustHide: Bool) {
         titleView.isHidden = mustHide
+        titleView.flex.markDirty()
         rootFlexContainer.flex.layout()
     }
     
     func hideLeftView(_ mustHide: Bool) {
         leftBarView.isHidden = mustHide
+        leftBarView.flex.markDirty()
         rootFlexContainer.flex.layout()
     }
     
