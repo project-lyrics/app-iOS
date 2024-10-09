cd73afab37c9be2a2895f817555a87ea2c5e3e95
Inwoo Hwang <dlsdn1207@gmail.com>
2024-10-10 03:40:30 +0900

    test: HomeExample에 테스트할 수 있는 VC & Mock 추가

--- Projects/Feature/Home/Testing/Sources/MockGetArtistNotesUseCase.swift
+++ Projects/Feature/Home/Testing/Sources/MockGetArtistNotesUseCase.swift
@@ -21,6 +21,7 @@ public class MockGetArtistNotesUseCase: GetArtistNotesUseCaseInterface {
         mustHaveLyrics: Bool
     ) -> AnyPublisher<[Note], NoteError> {
         if isInitial {
+            self.shouldStopPagination = false
             return Just(
                 [
                     Note(
@@ -63,18 +64,18 @@ public class MockGetArtistNotesUseCase: GetArtistNotesUseCaseInterface {
                             background: .black
                         ),
                         publisher: .init(
-                            id: 1,
+                            id: 2,
                             nickname: "테스트유저2",
                             profileCharacterType: .poopHair
                         ),
                         song: .init(
-                            id: 1,
+                            id: 2,
                             name: "테스트 노래2",
                             imageUrl: "1234.com",
                             artist: .init(
                                 id: 1,
                                 name: "테스트 아티스트2",
-                                imageSource: "www.com",
+                                imageSource: "",
                                 isFavorite: false
                             )
                         ),
@@ -93,18 +94,18 @@ public class MockGetArtistNotesUseCase: GetArtistNotesUseCaseInterface {
                             background: .beige
                         ),
                         publisher: .init(
-                            id: 1,
+                            id: 3,
                             nickname: "테스트유저3",
                             profileCharacterType: .poopHair
                         ),
                         song: .init(
-                            id: 1,
+                            id: 3,
                             name: "테스트 노래3",
-                            imageUrl: "1234.com",
+                            imageUrl: "",
                             artist: .init(
                                 id: 1,
                                 name: "테스트 아티스트3",
-                                imageSource: "www.com",
+                                imageSource: "",
                                 isFavorite: false
                             )
                         ),
@@ -143,7 +144,7 @@ public class MockGetArtistNotesUseCase: GetArtistNotesUseCaseInterface {
                                 artist: .init(
                                     id: 321,
                                     name: "검정치마",
-                                    imageSource: "www.com",
+                                    imageSource: "",
                                     isFavorite: false
                                 )
                             ),
@@ -169,11 +170,11 @@ public class MockGetArtistNotesUseCase: GetArtistNotesUseCaseInterface {
                             song: .init(
                                 id: 322,
                                 name: "테스트 노래2",
-                                imageUrl: "1234.com",
+                                imageUrl: "",
                                 artist: .init(
                                     id: 1,
                                     name: "테스트 아티스트2",
-                                    imageSource: "www.com",
+                                    imageSource: "",
                                     isFavorite: false
                                 )
                             ),
@@ -199,11 +200,11 @@ public class MockGetArtistNotesUseCase: GetArtistNotesUseCaseInterface {
                             song: .init(
                                 id: 323,
                                 name: "테스트 노래3",
-                                imageUrl: "1234.com",
+                                imageUrl: "",
                                 artist: .init(
                                     id: 1,
                                     name: "테스트 아티스트3",
-                                    imageSource: "www.com",
+                                    imageSource: "",
                                     isFavorite: false
                                 )
                             ),
@@ -211,7 +212,220 @@ public class MockGetArtistNotesUseCase: GetArtistNotesUseCaseInterface {
                             likesCount: 0,
                             isLiked: false,
                             isBookmarked: false
-                        )
+                        ),
+                        Note(
+                            id: 324,
+                            content: "테스트 내용입니다2.",
+                            status: .published,
+                            createdAt: .distantPast,
+                            lyrics: .init(
+                                content: "난 너의 아픔 속에 너의 고통 속에 기생\n하고 있는 아름다운 벌레야\n 이 밤이 지나고 우린 이상하지 않을까. 우린 어디서부터 잘못된 걸까 알고 싶지만 이제 더 이상 미련도 갖지 않은 내 자신이 이상해",
+                                background: .black
+                            ),
+                            publisher: .init(
+                                id: 324,
+                                nickname: "테스트유저2",
+                                profileCharacterType: .poopHair
+                            ),
+                            song: .init(
+                                id: 324,
+                                name: "테스트 노래2",
+                                imageUrl: "",
+                                artist: .init(
+                                    id: 1,
+                                    name: "테스트 아티스트2",
+                                    imageSource: "",
+                                    isFavorite: false
+                                )
+                            ),
+                            commentsCount: 0,
+                            likesCount: 0,
+                            isLiked: false,
+                            isBookmarked: false
+                        ),
+                        Note(
+                            id: 325,
+                            content: "테스트 내용입니다3.",
+                            status: .published,
+                            createdAt: .distantPast,
+                            lyrics: .init(
+                                content: "난 너의 아픔 속에 너의 고통",
+                                background: .beige
+                            ),
+                            publisher: .init(
+                                id: 325,
+                                nickname: "테스트유저3",
+                                profileCharacterType: .poopHair
+                            ),
+                            song: .init(
+                                id: 325,
+                                name: "테스트 노래3",
+                                imageUrl: "",
+                                artist: .init(
+                                    id: 1,
+                                    name: "테스트 아티스트3",
+                                    imageSource: "",
+                                    isFavorite: false
+                                )
+                            ),
+                            commentsCount: 0,
+                            likesCount: 0,
+                            isLiked: false,
+                            isBookmarked: false
+                        ),
+                        
+                        Note(
+                            id: 326,
+                            content: "테스트 추가 내용입니다333.테스트 내용입니다.테스트 내용입니다.테스트 내용입니다.테스트 내용입니다.테스트 내용입니다.테스트 내용입니다.테스트 내용입니다.테스트 내용입니다.테스트 내용입니다.테스트 내용입니다.테스트 내용입니다.테스트 내용입니다.테스트 내용입니다.테스트 내용입니다.테스트 내용입니다.테스트 내용입니다.테스트 내용입니다.테스트 내용입니다.테스트 내용입니다.테스트 내용입니다.테스트 내용입니다.테스트 내용입니다.테스트 내용입니다.테스트 내용입니다.테스트 내용입니다.테스트 내용입니다.테스트 내용입니다.테스트 내용입니다.",
+                            status: .published,
+                            createdAt: .distantPast,
+                            lyrics: .init(
+                                content: "난 너의 아픔 속에 너의 고통 속에 기생\n하고 있는 아름다운 벌레야",
+                                background: .skyblue
+                            ),
+                            publisher: .init(
+                                id: 326,
+                                nickname: "테스트유저1",
+                                profileCharacterType: .poopHair
+                            ),
+                            song: .init(
+                                id: 326,
+                                name: "everything",
+                                imageUrl: "https://i.scdn.co/image/ab67616d0000b2739c3a4e471c5e82a457dce2c0",
+                                artist: .init(
+                                    id: 326,
+                                    name: "검정치마",
+                                    imageSource: "",
+                                    isFavorite: false
+                                )
+                            ),
+                            commentsCount: 0,
+                            likesCount: 0,
+                            isLiked: false,
+                            isBookmarked: false
+                        ),
+                        Note(
+                            id: 326,
+                            content: "테스트 내용입니다2.",
+                            status: .published,
+                            createdAt: .distantPast,
+                            lyrics: .init(
+                                content: "난 너의 아픔 속에 너의 고통 속에 기생\n하고 있는 아름다운 벌레야\n 이 밤이 지나고 우린 이상하지 않을까. 우린 어디서부터 잘못된 걸까 알고 싶지만 이제 더 이상 미련도 갖지 않은 내 자신이 이상해",
+                                background: .black
+                            ),
+                            publisher: .init(
+                                id: 326,
+                                nickname: "테스트유저2",
+                                profileCharacterType: .poopHair
+                            ),
+                            song: .init(
+                                id: 326,
+                                name: "테스트 노래2",
+                                imageUrl: "",
+                                artist: .init(
+                                    id: 1,
+                                    name: "테스트 아티스트2",
+                                    imageSource: "",
+                                    isFavorite: false
+                                )
+                            ),
+                            commentsCount: 0,
+                            likesCount: 0,
+                            isLiked: false,
+                            isBookmarked: false
+                        ),
+                        Note(
+                            id: 327,
+                            content: "테스트 내용입니다2.",
+                            status: .published,
+                            createdAt: .distantPast,
+                            lyrics: .init(
+                                content: "난 너의 아픔 속에 너의 고통 속에 기생\n하고 있는 아름다운 벌레야\n 이 밤이 지나고 우린 이상하지 않을까. 우린 어디서부터 잘못된 걸까 알고 싶지만 이제 더 이상 미련도 갖지 않은 내 자신이 이상해",
+                                background: .black
+                            ),
+                            publisher: .init(
+                                id: 327,
+                                nickname: "테스트유저2",
+                                profileCharacterType: .poopHair
+                            ),
+                            song: .init(
+                                id: 327,
+                                name: "테스트 노래2",
+                                imageUrl: "",
+                                artist: .init(
+                                    id: 327,
+                                    name: "테스트 아티스트2",
+                                    imageSource: "",
+                                    isFavorite: false
+                                )
+                            ),
+                            commentsCount: 0,
+                            likesCount: 0,
+                            isLiked: false,
+                            isBookmarked: false
+                        ),
+                        Note(
+                            id: 328,
+                            content: "테스트 내용입니다3.",
+                            status: .published,
+                            createdAt: .distantPast,
+                            lyrics: .init(
+                                content: "난 너의 아픔 속에 너의 고통",
+                                background: .beige
+                            ),
+                            publisher: .init(
+                                id: 328,
+                                nickname: "테스트유저3",
+                                profileCharacterType: .poopHair
+                            ),
+                            song: .init(
+                                id: 328,
+                                name: "테스트 노래3",
+                                imageUrl: "",
+                                artist: .init(
+                                    id: 328,
+                                    name: "테스트 아티스트3",
+                                    imageSource: "",
+                                    isFavorite: false
+                                )
+                            ),
+                            commentsCount: 0,
+                            likesCount: 0,
+                            isLiked: false,
+                            isBookmarked: false
+                        ),
+                        
+                        Note(
+                            id: 329,
+                            content: "테스트 내용입니다3.",
+                            status: .published,
+                            createdAt: .distantPast,
+                            lyrics: .init(
+                                content: "난 너의 아픔 속에 너의 고통",
+                                background: .beige
+                            ),
+                            publisher: .init(
+                                id: 329,
+                                nickname: "테스트유저3",
+                                profileCharacterType: .poopHair
+                            ),
+                            song: .init(
+                                id: 329,
+                                name: "테스트 노래3",
+                                imageUrl: "",
+                                artist: .init(
+                                    id: 329,
+                                    name: "테스트 아티스트3",
+                                    imageSource: "",
+                                    isFavorite: false
+                                )
+                            ),
+                            commentsCount: 0,
+                            likesCount: 0,
+                            isLiked: false,
+                            isBookmarked: false
+                        ),
+                        
                     ]
                 )
                 .setFailureType(to: NoteError.self)
