d7b9f30e01e9cd0b22341daa15e90df7aeecfa7c
Inwoo Hwang <dlsdn1207@gmail.com>
2024-10-13 00:31:41 +0900

    feat: NoteCell에 link를 걸 수 있는 기능 추가
    - textView를 상속받아 link부분 외 다른 부분은 copy 및 다른 select기능을 할 수 없게 방지할 수 있는 커스텀 textview 추가
    - isHyperLinkTouchable flag를 활용하여 필요에 따라 hyperlink부분 터치 방지 또는 허용을 할 수 있도록 설정
    
    # Conflicts:
    #	Projects/Feature/Home/Interface/Sources/Views/Cells+SupplementaryViews/NoteCell.swift

---
+++ Projects/Shared/DesignSystem/Sources/Views/UnSelectableTextView.swift
@@ -0,0 +1,40 @@
+//
+//  UnSelectableTextView.swift
+//  SharedDesignSystem
+//
+//  Created by 황인우 on 10/12/24.
+//
+
+import UIKit
+
+public final class UnSelectableTextView: UITextView {
+    override public func point(inside point: CGPoint, with event: UIEvent?) -> Bool {
+        guard let pos = closestPosition(to: point) else { return false }
+        
+        guard let range = tokenizer.rangeEnclosingPosition(pos, with: .character, inDirection: .layout(.left)) else { return false }
+        
+        let startIndex = offset(from: beginningOfDocument, to: range.start)
+        
+        return attributedText.attribute(.link, at: startIndex, effectiveRange: nil) != nil
+    }
+    override public var selectedTextRange: UITextRange? {
+        get { return nil }
+        set {}
+    }
+    
+    override public func gestureRecognizerShouldBegin(_ gestureRecognizer: UIGestureRecognizer) -> Bool {
+        if gestureRecognizer is UIPanGestureRecognizer {
+            return super.gestureRecognizerShouldBegin(gestureRecognizer)
+        }
+        if let tapGestureRecognizer = gestureRecognizer as? UITapGestureRecognizer,
+           tapGestureRecognizer.numberOfTapsRequired == 1 {
+            return super.gestureRecognizerShouldBegin(gestureRecognizer)
+        }
+        if let longPressGestureRecognizer = gestureRecognizer as? UILongPressGestureRecognizer,
+           longPressGestureRecognizer.minimumPressDuration < 0.325 {
+            return super.gestureRecognizerShouldBegin(gestureRecognizer)
+        }
+        gestureRecognizer.isEnabled = false
+        return false
+    }
+}
