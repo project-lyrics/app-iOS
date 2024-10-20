0123155c8dc191612d9f6f80b3dc0dd3feec1c4c
derrickkim0109 <derrickkim0109@naver.com>
2024-10-20 12:03:23 +0900

    refactor: Notification 알림 리스트 개인, 전체로 API, UseCase 분리

---
+++ Projects/Domain/Notification/Interface/Sources/UseCases/GetPersonalNotificationUseCase.swift
@@ -0,0 +1,74 @@
+//
+//  GetPersonalNotificationUseCase.swift
+//  DomainNotification
+//
+//  Created by 황인우 on 10/5/24.
+//
+
+import Combine
+import Foundation
+
+import Core
+
+public protocol GetNotificationUseCaseInterface {
+    func execute(isInitial: Bool, perPage: Int) -> AnyPublisher<[NoteNotification], NotificationError>
+}
+
+public struct GetPersonalNotificationUseCase: GetNotificationUseCaseInterface {
+    private let notificationAPIService: NotificationAPIServiceInterface
+    private let notificationPaginationService: NotificationPaginationServiceInterface
+    
+    public init(
+        notificationAPIService: NotificationAPIServiceInterface,
+        notificationPaginationService: NotificationPaginationServiceInterface
+    ) {
+        self.notificationAPIService = notificationAPIService
+        self.notificationPaginationService = notificationPaginationService
+    }
+    
+    public func execute(
+        isInitial: Bool,
+        perPage: Int
+    ) -> AnyPublisher<[NoteNotification], NotificationError> {
+        if notificationPaginationService.isLoading {
+            return Empty()
+                .eraseToAnyPublisher()
+        }
+        
+        if isInitial {
+            self.notificationPaginationService.update(
+                currentPage: nil,
+                hasNextPage: true
+            )
+        }
+        
+        guard notificationPaginationService.hasNextPage else {
+            return Empty()
+                .eraseToAnyPublisher()
+        }
+        
+        notificationPaginationService.setLoading(true)
+        
+        return notificationAPIService.getPersonalNotifications(
+            currentPage: notificationPaginationService.currentPage,
+            numberOfNotifications: perPage
+        )
+        .receive(on: DispatchQueue.main)
+        .map({ [weak notificationPaginationService] getNotificationsResponse -> [NoteNotification] in
+            notificationPaginationService?.update(
+                currentPage: getNotificationsResponse.nextCursor,
+                hasNextPage: getNotificationsResponse.hasNext
+            )
+            notificationPaginationService?.setLoading(false)
+            
+            return getNotificationsResponse.data.map(NoteNotification.init)
+        })
+        .catch { [weak notificationPaginationService] error -> AnyPublisher<[NoteNotification], NotificationError> in
+            notificationPaginationService?.setLoading(false)
+            
+            return Fail(error: error)
+                .eraseToAnyPublisher()
+        }
+        .eraseToAnyPublisher()
+    }
+}
